e0889e2c8a772adf125e0fd6de392ea46a67b18a
Renato26sd <renatosusaya381@gmail.com>
2025-05-12 10:52:07 -0500

    Update cambio_moneda.py
    Agregado comentarios y ajustado el algoritmo de cambio de moneda

--- cambio_moneda.py
+++ cambio_moneda.py
@@ -1,3 +1,4 @@
+#Función que calcula cómo dar cambio con la menor cantidad de monedas/billetes
 def cambio_deMoneda(cantidad, denominaciones):
     denominaciones.sort(reverse=True)  
     resultado = {}
@@ -11,7 +12,7 @@ def cambio_deMoneda(cantidad, denominaciones):
 denominaciones = [500, 200, 100, 50, 20, 10, 5, 2, 1]
 cantidad = 786
 resultado = cambio_deMoneda(cantidad, denominaciones)
-
+# Imprimir el desglose del cambio en billetes/monedas
 print(f"Para entregar {cantidad} pesos, se debe dar:")
 for denom, num in resultado.items():
     print(f"{num} billete(s)/moneda(s) de {denom} pesos")
